aa415dfffc3f6a7664f51d32f5ab6ae83f3c016f
Anton Lazurko <antla@ciklum.com>
2025-04-28 17:30:09 +0300

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/assets/styles/globals.css'
+import NavBar from '@/components/NavBar';
 
 export const metadata = {
     title: 'Property App',
@@ -16,6 +17,7 @@ const MainLayout = ({ children }: IMainLayoutProps) => {
     return (
         <html lang="en">
             <body className="bg-gray-100 text-gray-900">
+                <NavBar />
                 <main className="container mx-auto">
                     {children}
                 </main>
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,13 +1,21 @@
 interface IPropertyPageParams {
     id: string;
 }
+interface IPropertyPageSearchParams {
+    name: string;
+}
 
 interface IPropertyPageProps {
     params: Promise<IPropertyPageParams>;
+    searchParams: Promise<IPropertyPageSearchParams>
 }
 
-const PropertyPage = async ({ params }: IPropertyPageProps) => {
+const PropertyPage = async ({ params, searchParams }: IPropertyPageProps) => {
     const { id } = await params;
+    const { name } = await searchParams;
+    console.log('id', id);
+    console.log('name', name);
+
     return <div>Property: #{id}</div>;
 };
 
