13ea366463af744cde34db0e52980441f5bd8471
Anton Lazurko <antla@ciklum.com>
2025-04-30 17:55:25 +0300

    feat; add recent properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => (<>
     <Hero />
     <InfoBoxes />
+    <HomeProperties />
     </>);
 export default HomePage;
