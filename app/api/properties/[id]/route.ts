360d016e55dfba4682ad3260124b96243e967f5f
Anton Lazurko <antla@ciklum.com>
2025-05-07 19:04:42 +0300

    fix: params type

--- app/api/properties/[id]/route.ts
+++ app/api/properties/[id]/route.ts
@@ -1,21 +1,28 @@
-import { NextRequest } from "next/server"
-import connectDB from "@/config/db"
-import Property from "@/models/Property"
-import { IProperty } from "@/types/property"
+import { NextRequest } from "next/server";
+import connectDB from "@/config/db";
+import Property from "@/models/Property";
+import { IProperty } from "@/types/property";
 
-export const GET = async (request: NextRequest, { params }: { params: { id: string } }): Promise<Response> => {
-    try {
-        await connectDB()
-        const property: IProperty | null = await Property.findById(params.id)
-        if (!property) {
-            return new Response("Property not found", { status: 404 })
-        }
-        return new Response(JSON.stringify(property), {
-            status: 200,
-            headers: { "Content-Type": "application/json" },
-        })
-    } catch (error) {
-        console.error("Error fetching property:", error)
-        return new Response("Failed to fetch property", { status: 500 })
-    }
+interface Params {
+  id: string;
 }
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: Params }
+): Promise<Response> => {
+  try {
+    await connectDB();
+    const property: IProperty | null = await Property.findById(params.id);
+    if (!property) {
+      return new Response("Property not found", { status: 404 });
+    }
+    return new Response(JSON.stringify(property), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching property:", error);
+    return new Response("Failed to fetch property", { status: 500 });
+  }
+};
