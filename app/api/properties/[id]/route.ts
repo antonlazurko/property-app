ce5b68a3f1ed15421e09be620ec1594abff7f9c1
Anton Lazurko <antla@ciklum.com>
2025-05-07 19:11:24 +0300

    fix: params type

--- app/api/properties/[id]/route.ts
+++ app/api/properties/[id]/route.ts
@@ -1,28 +1,26 @@
-import { NextRequest } from "next/server";
-import connectDB from "@/config/db";
-import Property from "@/models/Property";
-import { IProperty } from "@/types/property";
-
-interface Params {
-  id: string;
-}
+import { NextRequest } from "next/server"
+import connectDB from "@/config/db"
+import Property from "@/models/Property"
+import { IProperty } from "@/types/property"
 
 export const GET = async (
   request: NextRequest,
-  { params }: { params: Params }
+  context: { params: { id: string } }
 ): Promise<Response> => {
   try {
-    await connectDB();
-    const property: IProperty | null = await Property.findById(params.id);
+    await connectDB()
+    const property: IProperty | null = await Property.findById(context.params.id)
+
     if (!property) {
-      return new Response("Property not found", { status: 404 });
+      return new Response("Property not found", { status: 404 })
     }
+
     return new Response(JSON.stringify(property), {
       status: 200,
       headers: { "Content-Type": "application/json" },
-    });
+    })
   } catch (error) {
-    console.error("Error fetching property:", error);
-    return new Response("Failed to fetch property", { status: 500 });
+    console.error("Error fetching property:", error)
+    return new Response("Failed to fetch property", { status: 500 })
   }
-};
+}
