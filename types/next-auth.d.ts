5b5c425ecd3de30d1b1e3a5d794ccf596468f315
Anton Lazurko <antla@ciklum.com>
2025-05-09 12:21:09 +0300

    feat: add next-auth config

---
+++ types/next-auth.d.ts
@@ -0,0 +1,13 @@
+// // types/next-auth.d.ts
+
+// declare module "next-auth" {
+//   interface Session {
+//     accessToken?: string;
+//   }
+// }
+
+// declare module "next-auth/jwt" {
+//   interface JWT {
+//     accessToken?: string;
+//   }
+// }
