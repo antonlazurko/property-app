6699ab19ba9f7efacef838039b20cec13f0e85ec
Anton Lazurko <antla@ciklum.com>
2025-05-22 13:45:40 +0300

    feat: add global error page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -22,8 +22,11 @@ const PropertyPage = async ({ params }: IPropertyPageProps) => {
   try {
     property = await Property.findById(id).lean<IProperty>();
   } catch (error) {
-    console.error('error', error);
-    return notFound();
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error('Unknown error occurred');
+    }
   }
 
   if (!property) {
